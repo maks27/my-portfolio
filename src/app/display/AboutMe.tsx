d81ef78214dea3e41bbdefcd58b9f93f8c20183e
maks27 <maks2796@gmail.com>
2021-01-07 18:18:25 +0100

    changev2

--- src/app/display/AboutMe.tsx
+++ src/app/display/AboutMe.tsx
@@ -4,17 +4,15 @@ import { Container, Divider, Header } from "semantic-ui-react";
 const AboutMe = () => {
   return (
     <div className="sections">
-      <Container textAlign='left' >
+      <Container textAlign="justified" fluid>
         <Header size="large" className="text-center">
           O mnie
         </Header>
         <p>
-          Nazywam się Maksymilian Pliszczyński. 
-          Ukończyłem studnia informatyczne na Uniwersytecie Warmińsko-Mazurskim w Olsztynie. Aktualnie jestem w
-          trakcie poszukiwania pierwszej pracy w dziale IT. Najbardziej
-          chciałbym się rozwijać w kierunku Full-stack developer, ponieważ
-          zarówno tworzenie back-endu jak i front-endu strony sprawia mi wielką
-          satysfakcję.
+          Mam na imię Maksymilian Pliszczyński. Aktualnie jestem w trakcie
+          poszukiwania pierwszej pracy w dziale IT. Najbardziej chciałbym się
+          rozwijać w kierunku Full-stack developer, ponieważ zarówno tworzenie
+          back-endu jak i front-endu strony sprawia mi wielką satysfakcję.
         </p>
         <Divider />
       </Container>
--- src/app/display/Hobby.tsx
+++ src/app/display/Hobby.tsx
@@ -4,17 +4,16 @@ import { Container, Divider, Header } from "semantic-ui-react";
 const Hobby = () => {
   return (
     <div className="sections">
-      <Container textAlign="left" fluid>
+      <Container textAlign="justified" fluid>
         <Header size="large" className="text-center">
           Zainteresowania
         </Header>
         <p>
           Posiadam szeroki zakres zainteresowań. W wolnym czasie zajmuje się
           projektowaniem gry przy wykorzystaniu środowiska Unity, która mam
-          nadzieję, kiedyś doczeka się premiery. Oprócz tego moją pasją jest
-          muzyka, a w szczególności gra na gitarze. Interesuje się także
-          zwierzętami egzotycznymi takimi jak gekony lamparcie, których w tym
-          momencie posiadam, aż 10 sztuk.
+          nadzieję, kiedyś doczeka się premiery. Poza tym moją pasją jest muzyka, 
+          a w szczególności gra na gitarze. Interesuje się także zwierzętami egzotycznymi. 
+          Aktualnie hoduje gekony lamparcie. 
         </p>
         <Divider />
       </Container>

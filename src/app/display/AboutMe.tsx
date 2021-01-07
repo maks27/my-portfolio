089dd350022bc5ef2d8b648bb2086cec90c986c6
maks27 <maks2796@gmail.com>
2021-01-07 20:57:22 +0100

    changes text

--- src/app/display/AboutMe.tsx
+++ src/app/display/AboutMe.tsx
@@ -4,16 +4,16 @@ import { Container, Divider, Header } from "semantic-ui-react";
 const AboutMe = () => {
   return (
     <div className="sections">
-      <Container textAlign="justified" fluid>
-        <Header size="large" className="text-center">
+       <Header size="large" className="text-center">
           O mnie
-        </Header>
+      </Header>
+      <Container textAlign="justified" fluid>
         <p>
-          Mam na imię Maksymilian Pliszczyński. Aktualnie jestem w trakcie
-          poszukiwania pierwszej pracy w dziale IT. Najbardziej chciałbym się
+          Nazywam się Maksymilian Pliszczyński. Posiadam tytuł inżyniera, uzyskany na Uniwersytecie Warmińsko-Mazurskim w Olsztynie.
+          Aktualnie jestem w trakcie poszukiwania pierwszej pracy w dziale IT. Najbardziej chciałbym się
           rozwijać w kierunku Full-stack developer, ponieważ zarówno tworzenie
           back-endu jak i front-endu strony sprawia mi wielką satysfakcję.
-        </p>
+          </p>
         <Divider />
       </Container>
     </div>
